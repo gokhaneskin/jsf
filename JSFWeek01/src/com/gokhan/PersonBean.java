8e798bdec31f495c74cd41308ff53ba5db079a15
gokhan.eskin <gokhaneskin34@gmail.com>
2020-12-07 19:08:54 +0300

    first commit

---
+++ JSFWeek01/src/com/gokhan/PersonBean.java
@@ -0,0 +1,24 @@
+package com.gokhan;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class PersonBean {
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
